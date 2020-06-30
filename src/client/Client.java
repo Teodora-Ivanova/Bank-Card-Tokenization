508b5042e20dcfb499ea203627f1f01eff06297d
Blago24 <blago990@abv.bg>
2020-06-30 16:59:00 +0300

    test

--- src/client/Client.java
+++ src/client/Client.java
@@ -4,7 +4,7 @@ import client.gui.SignFrame;
 public class Client {
 
     public static void main(String[] args) {
-        
+        //test
         /*Start the client */
         java.awt.EventQueue.invokeLater(new Runnable() {
             public void run() {
