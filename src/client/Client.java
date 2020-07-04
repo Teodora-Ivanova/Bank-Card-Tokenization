e0dcf1736db6bf7f55051afd045991ee97f23d7c
Blagovest Konetsovski <blago990@abv.bg>
2020-07-04 13:49:27 +0300

    aide

--- src/client/Client.java
+++ src/client/Client.java
@@ -4,11 +4,9 @@ import client.gui.SignFrame;
 public class Client {
 
     public static void main(String[] args) {
-<<<<<<< HEAD
+// aide pak
         // ot tedi
-=======
         //test
->>>>>>> 6e93bb3637ef8f63eab84a1a7d01e3b2a74ea109
         /*Start the client */
         // its workin
         java.awt.EventQueue.invokeLater(new Runnable() {
