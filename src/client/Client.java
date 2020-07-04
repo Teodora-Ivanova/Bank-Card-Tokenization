334b86674d44f97c6fd6b8ade82cf028ae447031
Teodora Ivanova <tedi199962@gmail.com>
2020-07-04 13:55:22 +0300

    Ne znam

--- src/client/Client.java
+++ src/client/Client.java
@@ -4,11 +4,8 @@ import client.gui.SignFrame;
 public class Client {
 
     public static void main(String[] args) {
-<<<<<<< HEAD
         // ot tedi
-=======
         //test
->>>>>>> 6e93bb3637ef8f63eab84a1a7d01e3b2a74ea109
         /*Start the client */
         // its workin
         java.awt.EventQueue.invokeLater(new Runnable() {
