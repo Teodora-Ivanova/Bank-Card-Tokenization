8a5c9f4e836b9cc75ddc6cb598de2821faa37277
Teodora Ivanova <tedi199962@gmail.com>
2020-07-14 22:49:05 +0300

    Remove comments

--- src/Main.java
+++ src/Main.java
@@ -3,15 +3,10 @@ import server.Server;
 
 public class Main {
 
-    /**
-     *
-     * @param args
-     */
     public static void main(String[] args) {
-        /*Start the server*/
+
         new Thread(new Server(8080)).start();
-       
-        /*Start the client */
+        
         java.awt.EventQueue.invokeLater(new Runnable() {
             public void run() {
                 new SignFrame().setVisible(true);
