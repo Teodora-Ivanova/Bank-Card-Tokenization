3f2cd89704720ca2b095481a25958480d550bbfa
Teodora Ivanova <tedi199962@gmail.com>
2020-07-14 22:45:15 +0300

    Remove useless comments form class InvalidFirstNumber

--- src/exceptions/InvalidFirstNumber.java
+++ src/exceptions/InvalidFirstNumber.java
@@ -1,19 +1,10 @@
 package exceptions;
 
 public class InvalidFirstNumber extends InvalidCreditCardNumber{
-
-    /**
-     *
-     */
     public InvalidFirstNumber() {
     }
 
-    /**
-     *
-     * @param message
-     */
     public InvalidFirstNumber(String message) {
         super(message);
     }
-    
 }
