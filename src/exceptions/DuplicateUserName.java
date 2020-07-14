a167667351f0bfa0111fd16e6cded6d9ade56a1d
Teodora Ivanova <tedi199962@gmail.com>
2020-07-14 22:36:12 +0300

    Remove useless comments form class DuplicateUserName

--- src/exceptions/DuplicateUserName.java
+++ src/exceptions/DuplicateUserName.java
@@ -2,19 +2,10 @@ package exceptions;
 
 
 public class DuplicateUserName extends Exception{
-
-    /**
-     *
-     */
     public DuplicateUserName() {
     }
 
-    /**
-     *
-     * @param message
-     */
     public DuplicateUserName(String message) {
         super(message);
     }
-    
 }
