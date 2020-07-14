292f9438b2a4e896fc0713d609426e148da727e1
Teodora Ivanova <tedi199962@gmail.com>
2020-07-14 22:19:04 +0300

    Rename arguments names in compare method

--- src/comparators/CardComparator.java
+++ src/comparators/CardComparator.java
@@ -2,22 +2,21 @@ package comparators;
 
 import java.util.Comparator;
 import java.util.Map;
+
 import primitives.CreditCard;
 import primitives.Token;
 
 public class CardComparator implements Comparator<Token> {
-
     Map<Token, CreditCard> base;
-
 
     public CardComparator(Map<Token, CreditCard> base) {
         this.base = base;
     }
 
     @Override
-    public int compare(Token a, Token b) {
-        int result = base.get(a).compareTo(base.get(b));
-        if (result <= - 1) {
+    public int compare(Token t1, Token t2) {
+        int result = base.get(t1).compareTo(base.get(t2));
+        if (result <= -1) {
             return -1;
         }
         return 1;
