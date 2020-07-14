1f7a9961781d86bdba9c0efc8a1e836ab0d650ce
Teodora Ivanova <tedi199962@gmail.com>
2020-07-14 22:43:56 +0300

    Remove useless comments

--- src/exceptions/InvalidCreditCardNumber.java
+++ src/exceptions/InvalidCreditCardNumber.java
@@ -1,20 +1,11 @@
 package exceptions;
 
 public class InvalidCreditCardNumber extends Exception {
-
-    /**
-     *
-     */
     public InvalidCreditCardNumber() {
         super();
     }
 
-    /**
-     *
-     * @param message
-     */
     public InvalidCreditCardNumber(String message) {
         super(message);
     }
-
 }
