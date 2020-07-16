b50fbe060e6d3cba404badfb053322432dbfdd30
Teodora Ivanova <tedi199962@gmail.com>
2020-07-16 13:55:27 +0300

    Add one more user

--- test/primitives/EmptySignUpDataTest.java
+++ test/primitives/EmptySignUpDataTest.java
@@ -11,5 +11,6 @@ public class EmptySignUpDataTest {
     public void handleEmptySignUpData() throws Exception {
         User user1 = new User("Mariika","12345");
         User user2 = new User("Ivancho","1234567");
+        User user3 = new User("Gosho", "021235");
     }
 }
