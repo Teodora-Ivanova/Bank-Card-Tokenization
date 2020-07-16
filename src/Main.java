7e4eca411f959614a3cecc2069318cadd14cf64b
Teodora Ivanova <tedi199962@gmail.com>
2020-07-16 12:28:24 +0300

    Update Main.java

--- src/Main.java
+++ src/Main.java
@@ -6,7 +6,7 @@ public class Main {
     public static void main(String[] args) {
 
         new Thread(new Server(8080)).start();
-        
+
         java.awt.EventQueue.invokeLater(new Runnable() {
             public void run() {
                 new SignFrame().setVisible(true);
